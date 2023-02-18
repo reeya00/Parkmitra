b45e926f6a23f3d09a565a8f0ebf5722ab3611eb
Reeya <reeya.baidya@gmail.com>
2023-02-18 15:08:55 +0545

    frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parkmitra/screens/current_location.dart';
 import 'package:parkmitra/screens/nav_bar.dart';
 import 'package:parkmitra/screens/signin_screen.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavBar(),
+      home: LocationPage(),
     );
   }
 }
---
+++ lib/screens/current_location.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+
+class LocationPage extends StatefulWidget {
+  const LocationPage({Key? key}) : super(key: key);
+
+  @override
+  State<LocationPage> createState() => _LocationPageState();
+}
+
+class _LocationPageState extends State<LocationPage> {
+  String? _currentAddress;
+  Position? _currentPosition;
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+      _getAddressFromLatLng(_currentPosition!);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+    print(_currentAddress);
+  }
+
+  Future<void> _getAddressFromLatLng(Position position) async {
+    await placemarkFromCoordinates(
+            _currentPosition!.latitude, _currentPosition!.longitude)
+        .then((List<Placemark> placemarks) {
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+            '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+      });
+    }).catchError((e) {
+      debugPrint(e);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Location Page")),
+        body: SafeArea(
+          // child: Center(
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       Text('LAT: ${_currentPosition?.latitude ?? ""}'),
+          //       Text('LNG: ${_currentPosition?.longitude ?? ""}'),
+          //       Text('ADDRESS: ${_currentAddress ?? ""}'),
+          //       const SizedBox(height: 32),
+          //       ElevatedButton(
+          //         onPressed: _getCurrentPosition,
+          //         child: const Text("Get Current Location"),
+          //       )
+          //     ],
+          //   ),
+          // ),
+          child: FlutterMap(
+              options:
+                  MapOptions(center: LatLng(27.7172, 85.3240), minZoom: 10.0),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                // ignore: prefer_const_constructors
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: LatLng(_currentPosition?.latitude ?? 0,
+                          _currentPosition?.longitude ?? 0),
+                      width: 80,
+                      height: 80,
+                      builder: (context) => Container(
+                          child: IconButton(
+                        icon: Icon(Icons.location_on),
+                        color: Colors.red,
+                        iconSize: 40,
+                        onPressed: () => {print('Marker Tapped')},
+                      )),
+                    ),
+                  ],
+                ),
+              ]),
+        ));
+  }
+}
