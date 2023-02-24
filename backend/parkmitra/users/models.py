57c8b09d570742932aeff0407af951c9614ec854
monkeydsuyog <suyogsatyal45@gmail.com>
2023-02-24 11:26:51 +0545

    commit for reeya

--- backend/parkmitra/users/models.py
+++ backend/parkmitra/users/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    access_token = models.CharField(max_length=256, null=True, blank=False)
-    refresh_token = models.CharField(max_length=256, null=True, blank=False)
+    access_token = models.CharField(max_length=256, null=True, blank=True)
+    refresh_token = models.CharField(max_length=256, null=True, blank=True)
