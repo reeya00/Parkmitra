25fb67d8c024da3230a438dfa8957256ea26bc90
Reeya <reeya.baidya@gmail.com>
2023-02-25 11:31:14 +0545

    updates in active screen and profile screen

--- lib/screens/active_screen.dart
+++ lib/screens/active_screen.dart
@@ -2,14 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+
 class ActiveScreen extends StatelessWidget {
-  const ActiveScreen({super.key});
+  final String parkingTime='10';
+  final String exitTime='11';
+
+  // ActiveScreen({required this.parkingTime, required this.exitTime});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child : Center(
-      child: Text("Active page")),
+      height: 50,
+      color: Colors.grey[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'PARKED AT',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                parkingTime,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'EXIT AT',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                exitTime,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,15 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  List<Map<String, String>> _vehicles = [];
+
+  void _addVehicle() async {
+    Map<String, String>? newVehicle = await showDialog<Map<String, String>>(
+      context: context,
+      builder: (BuildContext context) {
+        String name = '';
+        String numberPlate = '';
+
+        return AlertDialog(
+          title: const Text('Add Vehicle'),
+          content: SizedBox(
+            height: 150,
+            child: Column(
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Vehicle Name',
+                  ),
+                  onChanged: (value) {
+                    name = value;
+                  },
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Number Plate',
+                  ),
+                  onChanged: (value) {
+                    numberPlate = value;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Map<String, String> newVehicle = {
+                  'name': name,
+                  'numberPlate': numberPlate,
+                };
+                Navigator.pop(context, newVehicle);
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (newVehicle != null) {
+      setState(() {
+        _vehicles.add(newVehicle);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Center(
-        child: Text("Profile Screen"),)
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: _vehicles.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(_vehicles[index]['name']!),
+            subtitle: Text(_vehicles[index]['numberPlate']!),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addVehicle,
+        tooltip: 'Add Vehicle',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
