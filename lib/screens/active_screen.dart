4b37e2841ea43bfd9e45a46f345feb7f33f74f6b
Axnusic <shresprbn@gmail.com>
2023-03-07 14:13:48 +0545

    active screen

--- lib/screens/active_screen.dart
+++ lib/screens/active_screen.dart
@@ -1,61 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-
+import 'package:get/get.dart';
+import 'qr_scanner.dart';
 
 class ActiveScreen extends StatelessWidget {
-  final String parkingTime='10';
-  final String exitTime='11';
-
-  // ActiveScreen({required this.parkingTime, required this.exitTime});
+  final String parkingTime = '10';
+  final String exitTime = '11';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      color: Colors.grey[200],
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'PARKED AT',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 4),
-              Text(
-                parkingTime,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Vehicle Details'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            decoration: BoxDecoration(
+              color: Colors.white24,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: TextButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      height: 200,
+                      child: Center(
+                        child: Text('This is a modal sheet.'),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Status', style: TextStyle(color: Colors.white)),
+            ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'EXIT AT',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey[600],
+        ],
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        leading: Text('Space ID'),
+                        title: Text('Vehicle'),
+                        subtitle: Text('entry time: 11, exit time: 12'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle "Pay Now" button press
+                          Get.to(() => QRCodeScanner());
+                        },
+                        child: Text('Pay Now'),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 4),
-              Text(
-                exitTime,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+              ],
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        leading: Text('Space ID'),
+                        title: Text('Vehicle'),
+                        subtitle: Text('entry time: 11, exit time: 12'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle "Pay Now" button press
+                        },
+                        child: Text('Pay Now'),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.only(right: 20, left: 20, top: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Expanded(
+                      child: ListTile(
+                        leading: Text('Space ID'),
+                        title: Text('Vehicle'),
+                        subtitle: Text('entry time: 11, exit time: 12'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle "Pay Now" button press
+                        },
+                        child: Text('Pay Now'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
