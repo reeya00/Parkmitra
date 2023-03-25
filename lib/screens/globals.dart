dcbdf427febf9b6e1ec746b32da788ea0cfeb9cf
monkeydsuyog <suyogsatyal45@gmail.com>
2023-03-25 23:10:17 +0545

    qr verify for entry and exit and esewa integration

--- lib/screens/globals.dart
+++ lib/screens/globals.dart
@@ -1,3 +1,2 @@
 // String baseUrl = 'https://f160-202-51-76-79.in.ngrok.io/';
-// String baseUrl = 'https://e755-110-44-115-170.in.ngrok.io/';
-String baseUrl = 'http://127.0.0.1:8000/';
+String baseUrl = 'https://6ff2-110-44-115-169.in.ngrok.io/';
